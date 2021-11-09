6abef3cbcb3c7b7fd692f25ded9ea071423f5133
Marco Tenti <marco.tenti@abd.it>
2021-11-09 10:08:37 +0100

    Sync: added cache_time_to_live_ms property and externalized config

--- nexu-standalone/src/main/java/lu/nowina/nexu/NexuLauncher.java
+++ nexu-standalone/src/main/java/lu/nowina/nexu/NexuLauncher.java
@@ -48,7 +48,7 @@ public class NexuLauncher {
 		launcher.launch(args);
 	}
 
-	protected void launch(String[] args) throws IOException {
+	public void launch(String[] args) throws IOException {
 		props = loadProperties();
 		loadAppConfig(props);
 
@@ -144,13 +144,15 @@ public class NexuLauncher {
 	private final Properties loadProperties() throws IOException {
 
         Properties props = new Properties();
-        
+		loadPropertiesFromClasspath(props);
+
         if (!loadPropertiesFromJarFolder(props)) {        
             loadPropertiesFromClasspath(props);
         }
 		return props;
+
 	}
-    
+	
     private boolean loadPropertiesFromJarFolder(Properties props) {
         // check location just outside jar file, to see if we have externalized properties there
         try {
@@ -176,7 +178,7 @@ public class NexuLauncher {
 		InputStream configFile = NexUApp.class.getClassLoader().getResourceAsStream("nexu-config.properties");
 		if (configFile != null) {
 			props.load(configFile);
-            logger.info("Loaded properties from inside JAR");
+			logger.info("Loaded properties from inside JAR");
 		}
 	}
 
