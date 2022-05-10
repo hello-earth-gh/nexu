bb7d9ccb4156b04fa2274a368fbc74fbeb3dc472
Marco Tenti <marco.tenti@abd.it>
2022-05-10 10:56:03 +0200

    update

--- nexu-app/run.bat
+++ nexu-app/run.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\openjdk\jdk-11.0.6+10_openj9-0.18.1\bin\java.exe" -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar target\nexu-app-1.23-SNAPSHOT.jar
+"C:\Program Files\Java\openjdk\jdk-11.0.6+10_openj9-0.18.1\bin\java.exe" -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -jar target\nexu-app-1.23-SNAPSHOT-jar-with-dependencies.jar
